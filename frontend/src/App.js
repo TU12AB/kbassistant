9aaa01f91aabe597ddb53ed24c366662de283622
Ankita banerjee <137812736+TU12AB@users.noreply.github.com>
2026-04-21 14:35:04 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -21,7 +21,7 @@ function App() {
     setLoading(true);
 
     try {
-      const res = await fetch("http://127.0.0.1:8000/chat", {
+      const res = await fetch("https://kbassistant-63e1.onrender.com/chat", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ messages: updatedMessages })
