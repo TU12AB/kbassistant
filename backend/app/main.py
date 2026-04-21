3025ec37be3d4ea8a49ae658312e13bd2a9a4d6e
Ankita banerjee <137812736+TU12AB@users.noreply.github.com>
2026-04-21 14:04:05 +0530

    back

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://kbassistant-sg45.vercel.app/"],
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
